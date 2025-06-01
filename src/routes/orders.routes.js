f850c09bba0740cd788c3a4b718a09bb96a30890
CEGB03 <cgomez453@alumnos.iua.edu.ar>
2025-06-01 12:14:48 -0300

    .

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get('/', getAllOrders); // Obtener todas las órdenes
 router.get('/:id', getOrderById); // Obtener orden por ID
-router.get('/:id', getOrdersByUser); // Obtener orden por ID
+router.get('/client=:id', getOrdersByUser); // Obtener orden por ID
 router.post('/newOrder', createOrder); // Crear orden
 router.put('/:id', updateOrder); // Editar orden
 router.delete('/:id', deleteOrder); // Eliminar orden
