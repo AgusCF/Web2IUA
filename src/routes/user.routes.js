7c8f13602468035a93aa3c3301d89140891a5849
CEGB03 <cgomez453@alumnos.iua.edu.ar>
2025-06-18 11:33:21 -0300

    .

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ import { verificarAdmin } from '../middleware/auth.js';
 const router = express.Router();
 
 // Aplica el middleware a todas las rutas de órdenes
-router.use( , verificarAdmin);
+router.use( verificarAdmin );
 
 router.get('/', getAllUsers);
 router.get('/by-tel', getUserByTel);
